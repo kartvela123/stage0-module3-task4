34809fe8dc7735b38571bd33a5be1eac054e05e1
kart <mkartveli9@gmail.com>
2023-02-16 14:02:21 +0000

    3.3

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a, b;
         a = b = 10;
-        System.out.println((9 * (a^2) - 5 * b + a - 7)* ((a + b - 4*a*b)/2));
+        int ans = (9 * (a^2) - 5 * b + a - 7) * ((a + b + 4*a*b)/2);
     }
 }
